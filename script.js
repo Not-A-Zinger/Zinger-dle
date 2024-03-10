32899e65c46eb4aff919afb7fa83f3228d90fb11
Not-A-Zinger <162855675+Not-A-Zinger@users.noreply.github.com>
2024-03-10 06:43:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function openAll(){
+    window.open("https://contexto.me/")
+    window.open("https://www.nytimes.com/games/wordle/index.html")
+    window.open("https://www.nytimes.com/games/connections")
+    window.open("https://globle-game.com/")
+    window.open("https://globle-capitals.com/")
+    window.open("https://travle.earth/")
+    window.open("https://actorle.com/")
+    window.open("https://framed.wtf/")
+    window.open("https://episode.wtf/")
+    window.open("https://guessthe.game/")
+    window.open("https://guesstheaudio.com/")
+    close()
+    //window.open("")
+    //window.open("")
+    //window.open("")
+}

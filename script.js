93fd5120949082d8a88c8da50f744f082f305cce
Not-A-Zinger <162855675+Not-A-Zinger@users.noreply.github.com>
2024-03-18 15:28:09 +0300

    Update script.js
    Added Actorle, Metazooa, and Linxicon

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ function openAll(){
     window.open("https://globle-game.com/")
     window.open("https://globle-capitals.com/")
     window.open("https://travle.earth/")
+    window.open("https://actorle.com/")
     window.open("https://framed.wtf/")
     window.open("https://episode.wtf/")
     window.open("https://guessthe.game/")
     window.open("https://guesstheaudio.com/")
+    window.open("https://metazooa.com")
+    window.open("https://linxicon.com")         
     close()
 }
